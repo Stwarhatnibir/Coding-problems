15b795987c0d608e2e2d69e27ead2d6c87a3e474
Stwarhatnibir <sanondodas@gmail.com>
2026-08-19 00:37:10 +0530

    leetcode 14

---
+++ Leetcode1.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Leetcode1 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = s.twoSum(nums, target);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
---
+++ Leetcode14.java
@@ -0,0 +1,36 @@
+public
+
+import java.util.Arrays;
+
+class Leetcode14 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String[] V = { "flower", "flow", "flight" };
+
+        System.out.println(s.longestCommonPrefix(V));
+    }
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] V) {
+        StringBuilder ans = new StringBuilder();
+
+        Arrays.sort(V);
+
+        String first = V[0];
+        String last = V[V.length - 1];
+
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+
+            ans.append(first.charAt(i));
+        }
+
+        return ans.toString();
+    }
+}{
+
+}
