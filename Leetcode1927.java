86d22f5b0892139ef806153f992bee5cd0fe8dda
Stwarhatnibir <sanondodas@gmail.com>
2026-08-23 11:52:39 +0530

    leetcode 1927

---
+++ Leetcode1927.java
@@ -0,0 +1,42 @@
+class Leetcode1927 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String num = "5023";
+
+        boolean result = solution.sumGame(num);
+
+        System.out.println(result);
+    }
+}
+
+class Solution {
+
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+
+        int diff = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            boolean firstHalf = i < n / 2;
+
+            if (num.charAt(i) == '?') {
+
+                count += firstHalf ? 1 : -1;
+
+            } else {
+
+                int digit = num.charAt(i) - '0';
+
+                diff += firstHalf ? digit : -digit;
+            }
+        }
+
+        return 2 * diff != -9 * count;
+    }
+}
